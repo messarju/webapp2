748f7886accb6513bdac95f3d9ea430313f7d374
messarju <39274314+messarju@users.noreply.github.com>
2019-01-11 15:36:49 +0800

    20190111073513

--- cmd/ytdl.php
+++ cmd/ytdl.php
@@ -13,13 +13,14 @@ if ($action && isset($action)) {
 // $arr = array('a' => 1, 'b' => 2, 'c' => 3, 'd' => 4, 'e' => 5);
 // echo json_encode($arr, JSON_PRETTY_PRINT);
 
+$YT_FILE="youtube-dl";
+$YT_LATEST="https://yt-dl.org/downloads/latest/youtube-dl";
+if(!file_exists($YT_FILE)){
+    exec( 'curl -L ' . escapeshellarg($YT_LATEST) . ' -o ' . escapeshellarg($YT_FILE) );
+}
+
 if($URL){
-    $YT_FILE="youtube-dl";
-    $YT_LATEST="https://yt-dl.org/downloads/latest/youtube-dl";
     // $YT_LATEST="file:///mnt/META/wrx/www/biojet1.github.io/local/notes.html";
-    if(!file_exists($YT_FILE)){
-        exec( 'curl -L ' . escapeshellarg($YT_LATEST) . ' -o ' . escapeshellarg($YT_FILE) );
-    }
     if(file_exists($YT_FILE)){
         $cmd = 'python ' . escapeshellarg($YT_FILE);
         if($action=='json'){
@@ -28,13 +29,15 @@ if($URL){
             $cmd = $cmd . ' -F';
         }
         $cmd = $cmd . ' ' . escapeshellarg($URL);
-        $out = system($cmd);
+        $out = shell_exec($cmd . ' 2>&1');
         $json = json_decode($out);
         $INFO = json_encode($json, JSON_PRETTY_PRINT);
     }
 }
 // print_r($action);
-
+if(!$INFO){
+    $INFO = shell_exec( 'python ' . escapeshellarg($YT_FILE) . ' --version  2>&1');
+}
 
 $FORM_URL=htmlentities($_SERVER['PHP_SELF']);
 $URL=htmlentities($URL);
@@ -43,6 +46,8 @@ $INFO=htmlentities($INFO);
 $xml = <<<EOD
 <html>
     <head>
+        <meta content="text/html; charset=UTF-8" http-equiv="content-type"/>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <title>Youtube-dl query</title>
     </head>
     <body>
@@ -67,8 +72,6 @@ EOD;
 // $doc->loadXML($xml);
 // echo $doc->saveXML();
 /*
-        <meta content="text/html; charset=UTF-8" http-equiv="content-type"/>
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
 
 */
 
